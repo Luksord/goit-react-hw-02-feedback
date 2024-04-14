f9c8112fdd2eff541bb05075f0726d8b472e4b4c
Łukasz Wątorski <watorski.info@gmail.com>
2024-04-14 17:51:09 +0200

    .

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types';
 import css from './Notification.module.css';
 
-export const Notification = ({ message }) => {
-  return <p className={css.notification}>{message}</p>;
-};
+export const Notification = ({ message }) => (
+  <p className={css.notification}>{message}</p>
+);
+
+// export const Notification = ({ message }) => {
+//   return <p className={css.notification}>{message}</p>;
+// };
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
